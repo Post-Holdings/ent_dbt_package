ec3a86971fe99c5e9689ffd5a53aa52bcf390bad
Parimal Patel <61436708+parimalposts@users.noreply.github.com>
2023-02-01 10:59:24 -0500

    Create dbt_tests_snapshots.sql

---
+++ snapshots/dbt_tests_snapshots.sql
@@ -0,0 +1,21 @@
+{% snapshot dbt_tests_snapshots %}
+
+{{
+    config(
+        tags=['tests'],
+        strategy='timestamp',
+        unique_key='test_name',
+        updated_at='snapshot_date'
+    )
+}}
+
+-- Set schema, since tests use custom schema on dev
+{% if  target.name in ["dev", "ci"] %}
+    {%- set test_schema = target.schema ~ '_tests' -%}
+{% else %}
+     {%- set test_schema = 'tests' -%}
+{% endif %}
+
+{{ generate_tests_stats(test_schema) }}
+
+{% endsnapshot %}

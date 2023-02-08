e628887e4fa7b56736868f38f77678b9ac316d11
harishjoshi280388 <61141849+harishjoshi280388@users.noreply.github.com>
2023-02-08 10:48:58 +0530

    Update lkp_exchange_rate_daily.sql

--- macros/phi/lkp/lkp_exchange_rate_daily.sql
+++ macros/phi/lkp/lkp_exchange_rate_daily.sql
@@ -1,9 +1,9 @@
 {%- macro lkp_exchange_rate_daily(CURR_FROM_CODE,CURR_TO_CODE,EFF_FROM_D,ALIAS) -%}
 (SELECT CURR_FROM_CODE,CURR_TO_CODE,EFF_FROM_D,CURR_CONV_RT,
 ROW_NUMBER() over (partition by CURR_FROM_CODE,CURR_TO_CODE,EFF_FROM_D order by CURR_FROM_CODE,CURR_TO_CODE,EFF_FROM_D ) AS ROW_NUM
-    FROM ( {{ ref('src_currency_exch_rate_dly_dim')}} ) ) {{ALIAS}}
+    FROM ( {{ ref('v_dim_exchange_rate_dly')}} ) ) {{ALIAS}}
     ON {{ALIAS}}.CURR_FROM_CODE = {{ CURR_FROM_CODE }}
     AND {{ALIAS}}.CURR_TO_CODE = {{ CURR_TO_CODE }}
-    AND {{ALIAS}}.EFF_FROM_D = {{ EFF_FROM_D }}
+    AND {{ALIAS}}.EFF_FROM_D = CASE WHEN {{ CURR_FROM_CODE }}={{ CURR_TO_CODE }} THEN '1900-01-01' ELSE {{ EFF_FROM_D }} END
     AND {{ALIAS}}.ROW_NUM =1
-{% endmacro %}
+{%- endmacro -%}

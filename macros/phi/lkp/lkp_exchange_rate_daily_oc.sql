6f21aade3381c75b0bfa7c7264517a037350bf32
harishjoshi280388 <61141849+harishjoshi280388@users.noreply.github.com>
2023-02-08 10:49:31 +0530

    Update lkp_exchange_rate_daily_oc.sql

--- macros/phi/lkp/lkp_exchange_rate_daily_oc.sql
+++ macros/phi/lkp/lkp_exchange_rate_daily_oc.sql
@@ -1,10 +1,10 @@
 {%- macro lkp_exchange_rate_daily_oc(SOURCE_SYSTEM,CURR_FROM_CODE,CURR_TO_CODE,EFF_FROM_D,ALIAS) -%}
 (SELECT SOURCE_SYSTEM,CURR_FROM_CODE,CURR_TO_CODE,EFF_FROM_D,CURR_CONV_RT,
 ROW_NUMBER() over (partition by SOURCE_SYSTEM,CURR_FROM_CODE,CURR_TO_CODE,EFF_FROM_D order by CURR_FROM_CODE,CURR_TO_CODE,EFF_FROM_D ) AS ROW_NUM
-    FROM ( {{ ref('src_currency_exch_rate_dly_dim_oc')}} ) ) {{ALIAS}}
-    ON {{ALIAS}}.SOURCE_SYSTEM = {{ SOURCE_SYSTEM }}	
+    FROM ( {{ ref('v_dim_exchange_rate_dly_oc')}} ) ) {{ALIAS}}
+    ON {{ALIAS}}.SOURCE_SYSTEM = {{ SOURCE_SYSTEM }}
     AND {{ALIAS}}.CURR_FROM_CODE = {{ CURR_FROM_CODE }}
     AND {{ALIAS}}.CURR_TO_CODE = {{ CURR_TO_CODE }}
-    AND {{ALIAS}}.EFF_FROM_D = {{ EFF_FROM_D }}
+    AND {{ALIAS}}.EFF_FROM_D = CASE WHEN {{ CURR_FROM_CODE }}={{ CURR_TO_CODE }} THEN '1900-01-01' ELSE {{ EFF_FROM_D }} END
     AND {{ALIAS}}.ROW_NUM =1
-{% endmacro %}
+{%- endmacro -%}

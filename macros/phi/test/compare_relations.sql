d1996fd2aef08ce6cc4a21f90f2e27977c45f04a
Parimal Patel <61436708+parimalposts@users.noreply.github.com>
2023-02-09 08:07:34 -0500

    removing extra whitespace

--- macros/phi/test/compare_relations.sql
+++ macros/phi/test/compare_relations.sql
@@ -3,29 +3,19 @@
 {% set column_names = dbt_utils.get_filtered_columns_in_relation(from=a_relation, except=exclude_columns) %}
 
 {% set column_selection %}
-
-  {% for column_name in column_names %} 
-    {{ adapter.quote(column_name) }} 
-    {% if not loop.last %}
-      , 
-    {% endif %} 
+  {% for column_name in column_names %}  {{ adapter.quote(column_name) }}     {% if not loop.last %}      ,     {% endif %} 
   {% endfor %}
-
 {% endset %}
 
 {% set a_query %}
 select
-
   {{ column_selection }}
-
 from {{ a_relation }}
 {% endset %}
 
 {% set b_query %}
 select
-
   {{ column_selection }}
-
 from {{ b_relation }}
 {% endset %}
 

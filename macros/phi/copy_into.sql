398c6ab91b44b724a14f9d5b72ed73c09f99041e
Parimal Patel <61436708+parimalposts@users.noreply.github.com>
2023-03-18 22:51:22 -0400

    Update copy_into.sql

--- macros/phi/copy_into.sql
+++ macros/phi/copy_into.sql
@@ -1,11 +1,12 @@
-{% macro copy_into(table_name,stage_db_name,stage_schema_name,stage,pattern,fileformat,skipheader) %}
+{% macro copy_into(table_name,stage_db_name,stage_schema_name,stage,pattern,fileformat,skipheader,onerror) %}
 
     {{ log("Loading data", True) }}
     BEGIN;
     COPY INTO {{ table_name }}
     FROM @{{ stage_db_name }}.{{ stage_schema_name }}.{{ stage }}  
     PATTERN='{{ pattern }}'
-    FILE_FORMAT = (FORMAT_NAME='{{ target.database }}.{{ stage_schema_name }}.{{ fileformat }}' SKIP_HEADER = {{ skipheader }});
+    FILE_FORMAT = (FORMAT_NAME='{{ target.database }}.{{ stage_schema_name }}.{{ fileformat }}' SKIP_HEADER = {{ skipheader }})
+    ON_ERROR = '{{ onerror }} ;
     COMMIT;
     {{ log("Loaded data", True) }}
 

e6763ea81a4ab1fd760ae297fa3f7a1cf6e04543
Parimal Patel <61436708+parimalposts@users.noreply.github.com>
2023-04-11 09:57:43 -0400

    Create src_test_results_history.sql

---
+++ models/phi/dbt_artifacts/src/src_test_results_history.sql
@@ -0,0 +1,49 @@
+
+
+with source as (
+
+    select * from {{ source('dbt_tests', 'test_results_history') }}
+
+),
+
+renamed as (
+
+    select
+        invocation_id,
+        node_id,
+        test_name,
+        test_name_long,
+        test_type,
+        model_refs,
+        source_refs,
+        test_severity_config,
+        execution_time_seconds,
+        test_result,
+        file_test_defined,
+        test_table_name,
+        table_a_name,
+        table_b_name,
+        table_a_count,
+        table_b_count,
+        count_diff,
+        col_1_name,
+        table_a_col_1_value,
+        table_b_col_1_value,
+        col_1_diff,
+        col_2_name,
+        table_a_col_2_value,
+        table_b_col_2_value,
+        col_2_diff,
+        col_3_name,
+        table_a_col_3_value,
+        table_b_col_3_value,
+        col_3_diff,
+        test_status,
+        variance_pct_threshold,
+        _timestamp
+
+    from source
+
+)
+
+select * from renamed

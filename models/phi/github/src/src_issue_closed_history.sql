4b977d2325a11613bdec7754e291d899fde61b6b
Parimal Patel <61436708+parimalposts@users.noreply.github.com>
2023-04-13 18:10:58 -0400

    Create src_issue_closed_history.sql

---
+++ models/phi/github/src/src_issue_closed_history.sql
@@ -0,0 +1,23 @@
+
+
+with source as (
+
+    select * from {{ source('dbt_github', 'issue_closed_history') }}
+
+),
+
+renamed as (
+
+    select
+        issue_id,
+        updated_at,
+        closed,
+        actor_id,
+        commit_sha,
+        _fivetran_synced
+
+    from source
+
+)
+
+select * from renamed

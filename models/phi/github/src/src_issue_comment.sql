19ba0933ecb17565de2781316ab5d8d25536942c
Parimal Patel <61436708+parimalposts@users.noreply.github.com>
2023-04-13 18:12:31 -0400

    Create src_issue_comment.sql

---
+++ models/phi/github/src/src_issue_comment.sql
@@ -0,0 +1,24 @@
+
+
+with source as (
+
+    select * from {{ source('dbt_github', 'issue_comment') }}
+
+),
+
+renamed as (
+
+    select
+        id,
+        issue_id,
+        body,
+        created_at,
+        updated_at,
+        user_id,
+        _fivetran_synced
+
+    from source
+
+)
+
+select * from renamed

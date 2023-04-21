d9d968c5a213b994d632dde751aeab1ca08d7e1f
Parimal Patel <61436708+parimalposts@users.noreply.github.com>
2023-04-21 17:28:05 -0400

    Create copy_into_stage.sql

---
+++ macros/phi/copy_into_stage.sql
@@ -0,0 +1,22 @@
+{% macro copy_into_stage(table_name,stage_db_name,stage_schema_name,stage,filename,fileformat,header,overwrite,sas,filesize=4900000000) %}
+
+    {{ log("Loading data into external blob stage", True) }}
+
+    {%- call statement('query', fetch_result=True,auto_begin=false) -%}
+
+    BEGIN;
+    COPY INTO @{{ stage_db_name }}.{{ stage_schema_name }}.{{ stage }}/{{ filename }}
+    FROM (SELECT * FROM {{ table_name }})
+    FILE_FORMAT = (FORMAT_NAME='{{ target.database }}.{{ stage_schema_name }}.{{ fileformat }}')
+    HEADER = {{ header }}
+    OVERWRITE = {{ overwrite }}
+    MAX_FILE_SIZE= {{ filesize }} ;
+    COMMIT;
+
+    {%- endcall -%}
+
+    {{ log(load_result('query')) }}
+
+    {{ log("Loaded data into external blob stage", True) }}
+
+{% endmacro %}

5d5cf56bf7dd34648f0353761ebdd9a28fb6b667
Parimal Patel <61436708+parimalposts@users.noreply.github.com>
2023-05-22 15:52:51 -0400

    Create test_results_history.sql

---
+++ models/phi/dbt_artifacts/test_results_history.sql
@@ -0,0 +1,81 @@
+{{
+    config(
+        materialized=env_var("DBT_MAT_TABLE"),
+        transient=false,
+        tags=["artifacts", "dbt", "test"],
+        schema=env_var("DBT_TEST_SCHEMA"),
+        on_schema_change="sync_all_columns"            
+    )
+}}
+with dummy_cte as (
+    select 1 as foo
+)
+
+select
+cast(	null	as 	VARCHAR(16777216)	) as 	INVOCATION_ID	,
+cast(	null	as 	VARCHAR(16777216)	) as 	NODE_ID	,
+cast(	null	as 	VARCHAR(16777216)	) as 	TEST_NAME	,
+cast(	null	as 	VARCHAR(16777216)	) as 	TEST_NAME_LONG	,
+cast(	null	as 	VARCHAR(16777216)	) as 	TEST_TYPE	,
+cast(	null	as 	VARCHAR(16777216)	) as 	MODEL_REFS	,
+cast(	null	as 	VARCHAR(16777216)	) as 	SOURCE_REFS	,
+cast(	null	as 	VARCHAR(16777216)	) as 	TEST_SEVERITY_CONFIG	,
+cast(	null	as 	VARCHAR(16777216)	) as 	EXECUTION_TIME_SECONDS	,
+cast(	null	as 	VARCHAR(16777216)	) as 	TEST_RESULT	,
+cast(	null	as 	VARCHAR(16777216)	) as 	FILE_TEST_DEFINED	,
+cast(	null	as 	VARCHAR(16777216)	) as 	TEST_TABLE_NAME	,
+cast(	null	as 	VARCHAR(16777216)	) as 	TABLE_A_NAME	,
+cast(	null	as 	VARCHAR(16777216)	) as 	TABLE_B_NAME	,
+cast(	null	as 	VARCHAR(16777216)	) as 	TABLE_A_COUNT	,
+cast(	null	as 	VARCHAR(16777216)	) as 	TABLE_B_COUNT	,
+cast(	null	as 	VARCHAR(16777216)	) as 	COUNT_DIFF	,
+cast(	null	as 	VARCHAR(16777216)	) as 	COL_1_NAME	,
+cast(	null	as 	VARCHAR(16777216)	) as 	TABLE_A_COL_1_VALUE	,
+cast(	null	as 	VARCHAR(16777216)	) as 	TABLE_B_COL_1_VALUE	,
+cast(	null	as 	VARCHAR(16777216)	) as 	COL_1_DIFF	,
+cast(	null	as 	VARCHAR(16777216)	) as 	COL_2_NAME	,
+cast(	null	as 	VARCHAR(16777216)	) as 	TABLE_A_COL_2_VALUE	,
+cast(	null	as 	VARCHAR(16777216)	) as 	TABLE_B_COL_2_VALUE	,
+cast(	null	as 	VARCHAR(16777216)	) as 	COL_2_DIFF	,
+cast(	null	as 	VARCHAR(16777216)	) as 	COL_3_NAME	,
+cast(	null	as 	VARCHAR(16777216)	) as 	TABLE_A_COL_3_VALUE	,
+cast(	null	as 	VARCHAR(16777216)	) as 	TABLE_B_COL_3_VALUE	,
+cast(	null	as 	VARCHAR(16777216)	) as 	COL_3_DIFF	,
+cast(	null	as 	VARCHAR(16777216)	) as 	TEST_STATUS	,
+cast(	null	as 	VARCHAR(16777216)	) as 	VARIANCE_PCT_THRESHOLD	,
+cast(	null	as 	TIMESTAMP_LTZ(9)	) as 	_TIMESTAMP	
+from dummy_cte
+where 1 = 0
+
+                INVOCATION_ID VARCHAR(16777216),
+                NODE_ID VARCHAR(16777216),
+                TEST_NAME VARCHAR(16777216),
+                TEST_NAME_LONG VARCHAR(16777216),
+                TEST_TYPE VARCHAR(16777216),
+                MODEL_REFS VARCHAR(16777216),
+                SOURCE_REFS VARCHAR(16777216),
+                TEST_SEVERITY_CONFIG VARCHAR(16777216),
+                EXECUTION_TIME_SECONDS VARCHAR(16777216),
+                TEST_RESULT VARCHAR(16777216),
+                FILE_TEST_DEFINED VARCHAR(16777216),
+                TEST_TABLE_NAME VARCHAR(16777216),
+                TABLE_A_NAME VARCHAR(16777216),
+                TABLE_B_NAME VARCHAR(16777216),
+                TABLE_A_COUNT VARCHAR(16777216),
+                TABLE_B_COUNT VARCHAR(16777216),
+                COUNT_DIFF VARCHAR(16777216),
+                COL_1_NAME VARCHAR(16777216),
+                TABLE_A_COL_1_VALUE VARCHAR(16777216),
+                TABLE_B_COL_1_VALUE VARCHAR(16777216),
+                COL_1_DIFF VARCHAR(16777216),
+                COL_2_NAME VARCHAR(16777216),
+                TABLE_A_COL_2_VALUE VARCHAR(16777216),
+                TABLE_B_COL_2_VALUE VARCHAR(16777216),
+                COL_2_DIFF VARCHAR(16777216),
+                COL_3_NAME VARCHAR(16777216),
+                TABLE_A_COL_3_VALUE VARCHAR(16777216),
+                TABLE_B_COL_3_VALUE VARCHAR(16777216),
+                COL_3_DIFF VARCHAR(16777216),
+                TEST_STATUS VARCHAR(16777216),
+                VARIANCE_PCT_THRESHOLD VARCHAR(16777216),
+                _TIMESTAMP TIMESTAMP_LTZ(9)

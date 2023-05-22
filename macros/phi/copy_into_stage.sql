be74c3be4a6731f669ab176d41fef9ccf8564699
Parimal Patel <61436708+parimalposts@users.noreply.github.com>
2023-05-22 15:42:52 -0400

    Update copy_into_stage.sql

--- macros/phi/copy_into_stage.sql
+++ macros/phi/copy_into_stage.sql
@@ -1,4 +1,4 @@
-{% macro copy_into_stage(table_name,stage_db_name,stage_schema_name,stage,filename,fileformat,header,overwrite,sas,filesize=4900000000,single=true) %}
+{% macro copy_into_stage(table_name,stage_db_name,stage_schema_name,stage,filename,fileformat,header,overwrite,filesize=4900000000,single=true) %}
 
     {{ log("Loading data into external blob stage", True) }}
 
